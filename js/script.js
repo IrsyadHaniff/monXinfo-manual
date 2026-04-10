957656ff04e3e6819f649887c03bd7af22b0a450
IrsyadHaniff <irsyadhanif332@gmail.com>
2026-04-10 17:34:50 +0700

    disable klik kanan

--- js/script.js
+++ js/script.js
@@ -1,48 +1,51 @@
 // Logika dark/light mode
-document.addEventListener('DOMContentLoaded', () => {
-    const themeToggle = document.getElementById('theme-toggle');
-    const sunIcon = document.getElementById('icon-sun');
-    const moonIcon = document.getElementById('icon-moon');
+document.addEventListener("DOMContentLoaded", () => {
+  const themeToggle = document.getElementById("theme-toggle");
+  const sunIcon = document.getElementById("icon-sun");
+  const moonIcon = document.getElementById("icon-moon");
 
-    // Init Theme
-    if (localStorage.getItem('theme') === 'light') {
-        document.documentElement.classList.remove('dark');
-        updateIcons(false);
-    } else {
-        document.documentElement.classList.add('dark');
-        updateIcons(true);
-    }
+  // Init Theme
+  if (localStorage.getItem("theme") === "light") {
+    document.documentElement.classList.remove("dark");
+    updateIcons(false);
+  } else {
+    document.documentElement.classList.add("dark");
+    updateIcons(true);
+  }
 
-    themeToggle.addEventListener('click', () => {
-        const isDark = document.documentElement.classList.toggle('dark');
-        localStorage.setItem('theme', isDark ? 'dark' : 'light');
-        updateIcons(isDark);
-    });
+  themeToggle.addEventListener("click", () => {
+    const isDark = document.documentElement.classList.toggle("dark");
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+    updateIcons(isDark);
+  });
 
-    function updateIcons(isDark) {
-        sunIcon.classList.toggle('hidden', !isDark);
-        moonIcon.classList.toggle('hidden', isDark);
-    }
+  function updateIcons(isDark) {
+    sunIcon.classList.toggle("hidden", !isDark);
+    moonIcon.classList.toggle("hidden", isDark);
+  }
 });
 
 // Logika Page Loader
-window.addEventListener('load', () => {
-    const loader = document.getElementById('page-loader');
-    
-    // delay
-    setTimeout(() => {
-        loader.classList.add('hidden');
-    }, 600); 
+window.addEventListener("load", () => {
+  const loader = document.getElementById("page-loader");
+
+  // delay
+  setTimeout(() => {
+    loader.classList.add("hidden");
+  }, 600);
 });
 
 // Logika running title tab
 let text = "   monXinfo - Homepage   ";
-        let i = 0;
+let i = 0;
 
-        function scrollTitle() {
-            document.title = text.substring(i) + text.substring(0, i);
-            i = (i + 1) % text.length;
-            setTimeout(scrollTitle, 300);
-        }
+function scrollTitle() {
+  document.title = text.substring(i) + text.substring(0, i);
+  i = (i + 1) % text.length;
+  setTimeout(scrollTitle, 300);
+}
 
-        scrollTitle();
+scrollTitle();
+
+//non-aktifkan klick kanan
+document.addEventListener("contextmenu", (e) => e.preventDefault());
